47376b79fd939a88e7941fe17cb3ac1f41ebb614
liamjdavis <66973153+liamjdavis@users.noreply.github.com>
2023-07-13 16:32:04 -0700

    Add files via upload

--- detector.py
+++ detector.py
@@ -1,27 +1,16 @@
+import joblib
+from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+model = joblib.load('detector.joblib')
 
-df = pd.read_csv('news.csv')
-labels = df.label
+# load article as csv
+df = pd.read_csv('fake news story.csv')
 
-articleText = df['text']
-
-# text feature extraction with bag of words
+# transform article
 vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(articleText)
-featureNames = vectorizer.get_feature_names_out()
+X = vectorizer.fit_transform(df)
 
-# split into training and testing
-x_train,x_test,y_train,y_test=train_test_split(X, labels, test_size=0.2, random_state=7)
-
-# logistic regression
-model = LogisticRegression(max_iter=15000)
-model.fit(X, labels)
-
-# evaluate model
-accuracy = accuracy_score(y_true=y_test, y_pred=model.predict(x_test))
-print(accuracy)
+# test article
+predictions = model.predict(X)
+print(predictions)
