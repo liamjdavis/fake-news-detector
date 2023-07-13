f25b9482ab62d32e8a6687a0141f2e754a583b8d
liamjdavis <66973153+liamjdavis@users.noreply.github.com>
2023-07-13 16:58:39 -0700

    Add files via upload

--- detector.py
+++ detector.py
@@ -1,6 +1,6 @@
 import joblib
-from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
 
 model = joblib.load('detector.joblib')
 
@@ -9,8 +9,8 @@ df = pd.read_csv('fake news story.csv')
 
 # transform article
 vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(df)
+text = vectorizer.fit_transform(df)
 
 # test article
-predictions = model.predict(X)
+predictions = model.predict(text)
 print(predictions)
